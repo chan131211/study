52834dfbc5cde3fc0f7b494b635177f80214fe25
chan <1123528651@qq.com>
2020-06-03 16:37:46 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {Button, message, Calendar} from 'antd'
+export default class App extends Component {
+    clickHandler = () => {
+        message.success('添加成功')
+    }
+    onPanelChange = (value, mode) => {
+        console.log(value, mode)
+    }
+    render() {
+        return (
+            <div>
+                <h1>后台首页</h1>
+                <Button type="primary" onClick={this.clickHandler}>Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="danger">Danger</Button>
+                <Button type="link">Link</Button>
+                <Calendar onPanelChange={this.onPanelChange} />
+            </div>
+        )
+    }
+}
